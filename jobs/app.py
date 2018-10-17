cb242c2be1e54f9b0babcfcbe613e49e2c0711bc
Yeneneh Yimer <yeyimer@starbucks.com>
2018-10-17 09:11:45 -0700

    module3

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,7 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql,value=(),commit=False,single=False):
+def execute_sql(sql,values=(),commit=False,single=False):
     connection = open_connection()
     cursor = connection.execute(sql,values)
     if commit == True:
