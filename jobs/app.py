cd9045c64622da1f92b8b91f7b7dbe21c33721d1
Yeneneh Yimer <yeyimer@starbucks.com>
2018-10-18 10:13:25 -0700

    fix

--- jobs/app.py
+++ jobs/app.py
@@ -29,7 +29,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description,
-    job.salary, employer.id as employer_id, employer.name as emplayer_name
-    FROM job JOIN employer ON employer_id=job.employer_id')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as emplayer_name FROM job JOIN employer ON employer_id=job.employer_id')
     return render_template('index.html',jobs=jobs)
