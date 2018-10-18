e65f2ecee050e9d134dced7e9d38fc26c17d45d3
Yeneneh Yimer <yeyimer@starbucks.com>
2018-10-18 10:07:54 -0700

    module 4

--- jobs/app.py
+++ jobs/app.py
@@ -4,15 +4,15 @@ from flask import Flask, render_template, g
 PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 def open_connection():
-    connection = getattr(g,'_connection',None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = g._connection= sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql,values=(),commit=False,single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.execute(sql,values)
+    cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
@@ -29,7 +29,7 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT 'job.id, job.title, job.description,
+    jobs = execute_sql('SELECT job.id, job.title, job.description,
     job.salary, employer.id as employer_id, employer.name as emplayer_name
     FROM job JOIN employer ON employer_id=job.employer_id')
     return render_template('index.html',jobs=jobs)
